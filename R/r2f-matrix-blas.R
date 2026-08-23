429ae567716a2d100e6b68f181dbd722b3795674
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-08-23 14:54:23 -0400

    Materialize SYRK operands before shape guards

--- R/r2f-matrix-blas.R
+++ R/r2f-matrix-blas.R
@@ -543,6 +543,8 @@ syrk <- function(
     k <- x_dims$cols
   }
   lda <- x_dims$rows
+  X_name <- ensure_blas_operand_name(X, hoist)
+  X <- Fortran(X_name, X@value)
   assert_nonempty_blas_output(
     n,
     X,
@@ -551,7 +553,6 @@ syrk <- function(
     hoist,
     scope
   )
-  X_name <- ensure_blas_operand_name(X, hoist)
 
   # Output is symmetric n x n matrix
   writes_to_dest <- FALSE
