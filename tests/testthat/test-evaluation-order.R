5dd7c1733e7cdaa96abb7f2d55e687230369a83c
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-08-30 08:32:35 -0400

    Run runif order regression on CRAN

--- tests/testthat/test-evaluation-order.R
+++ tests/testthat/test-evaluation-order.R
@@ -72,6 +72,23 @@ test_that("cbind/rbind evaluate effectful arguments from left to right", {
   expect_identical(quick(rbind_fn)(), rbind_expected)
 })
 
+test_that("runif snapshots its minimum before evaluating its maximum", {
+  fn <- function(x) {
+    declare(type(x = double(1)))
+    bump <- function() {
+      x <<- x + 10
+      5
+    }
+    runif(1L, min = x, max = bump())
+  }
+  qfn <- quick(fn)
+
+  expect_identical(
+    set_seed_and_call(fn, 1),
+    set_seed_and_call(qfn, 1)
+  )
+})
+
 test_that("later subscript effects cannot change the evaluated base", {
   fn <- function(x) {
     declare(type(x = double(2)))
