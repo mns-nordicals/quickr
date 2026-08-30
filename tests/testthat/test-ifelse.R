2f4b5af1d39928b64281956ddab80031745a8630
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-08-30 05:04:44 -0400

    Defer ifelse branch mode errors

--- tests/testthat/test-ifelse.R
+++ tests/testthat/test-ifelse.R
@@ -212,6 +212,26 @@ test_that("ifelse defers anonymous local closure diagnostics", {
   expect_error(qreached(), "missing required argument")
 })
 
+test_that("ifelse defers unary mode errors in unselected branches", {
+  skipped_yes <- function() {
+    ifelse(FALSE, !1, TRUE)
+  }
+  skipped_no <- function() {
+    ifelse(TRUE, TRUE, !1)
+  }
+  reached_yes <- function() {
+    ifelse(TRUE, !1, TRUE)
+  }
+  reached_no <- function() {
+    ifelse(FALSE, TRUE, !1)
+  }
+
+  expect_quick_identical(skipped_yes, list())
+  expect_quick_identical(skipped_no, list())
+  expect_error(quick(reached_yes)(), "expects a logical value", fixed = TRUE)
+  expect_error(quick(reached_no)(), "expects a logical value", fixed = TRUE)
+})
+
 test_that("ifelse defers shape errors in unselected branches", {
   fn <- function(test) {
     declare(type(test = logical(3)))
