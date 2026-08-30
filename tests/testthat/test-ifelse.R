73ee3683edda7b6042b93609980ccb81e5e93150
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-08-30 06:38:42 -0400

    Revert "Defer skipped local closure diagnostics"
    This reverts commit 5d612b8a46a289be51727dca7bc1cad83f4f4754.

--- tests/testthat/test-ifelse.R
+++ tests/testthat/test-ifelse.R
@@ -326,19 +326,6 @@ test_that("valueless ifelse branches are deferred until selected", {
   )
 })
 
-test_that("ifelse defers anonymous local closure diagnostics", {
-  skipped <- function() {
-    ifelse(FALSE, (function(x) x + 1)(), 1)
-  }
-  reached <- function() {
-    ifelse(TRUE, (function(x) x + 1)(), 1)
-  }
-
-  expect_quick_identical(skipped, list())
-  qreached <- quick(reached)
-  expect_error(qreached(), "missing required argument")
-})
-
 test_that("ifelse defers shape errors in unselected branches", {
   fn <- function(test) {
     declare(type(test = logical(3)))
