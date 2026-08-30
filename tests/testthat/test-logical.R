350c7a6e719e0d0d74ea745eb29d7471c55be69f
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-08-30 04:48:10 -0400

    Defer supported intrinsic arity errors

--- tests/testthat/test-logical.R
+++ tests/testthat/test-logical.R
@@ -289,15 +289,56 @@ test_that("short-circuited division is not evaluated eagerly", {
 })
 
 test_that("short-circuited calls defer arity errors", {
-  skipped_abs <- function() {
-    FALSE && abs()
-  }
-  reached_abs <- function() {
-    TRUE && abs()
+  unary_intrinsics <- c(
+    "sin",
+    "cos",
+    "tan",
+    "tanh",
+    "asin",
+    "acos",
+    "atan",
+    "sqrt",
+    "exp",
+    "log",
+    "floor",
+    "ceiling",
+    "trunc",
+    "log10",
+    "abs",
+    "Re",
+    "Im",
+    "Mod",
+    "Arg",
+    "Conj",
+    "as.double",
+    "as.integer",
+    "cat",
+    "dim",
+    "drop",
+    "is.null",
+    "length",
+    "ncol",
+    "nrow",
+    "print",
+    "rev",
+    "seq_along",
+    "seq_len",
+    "t",
+    "which.max",
+    "which.min"
+  )
+  bad_calls <- lapply(unary_intrinsics, \(op) as.call(list(as.name(op))))
+  skipped <- function() NULL
+  body(skipped) <- as.call(c(
+    quote(`{`),
+    list(as.call(c(quote(c), lapply(bad_calls, \(x) call("&&", FALSE, x)))))
+  ))
+  reached <- function() {
+    TRUE && sqrt()
   }
 
-  expect_quick_identical(skipped_abs, list())
-  expect_error(quick(reached_abs)(), "abs")
+  expect_quick_identical(skipped, list())
+  expect_error(quick(reached)(), "sqrt")
 })
 
 test_that("short-circuited operators defer arity errors", {
@@ -317,7 +358,14 @@ test_that("short-circuited operators defer arity errors", {
       "!=",
       "+",
       "-",
-      "*"
+      "*",
+      "/",
+      "^",
+      "%%",
+      "%/%",
+      "%*%",
+      "%o%",
+      ":"
     ),
     \(op) as.call(list(as.name(op)))
   )
